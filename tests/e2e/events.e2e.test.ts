37fd19c1339dd76fdf012c95a915841c0a7e7a7f
Adriana Paun <adrianapaun@192-168-0-106.rdsnet.ro>
2022-10-27 19:57:34 +0300

    added e2e tests for events resource

---
+++ tests/e2e/events.e2e.test.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const domain = 'http://localhost:3000'; //make sue to update with porper domain
+const resource = '/events';
+
+describe("/events", () => {
+    it("POST /events with success", async () => {
+        const body = [
+            {
+                "name": "blue",
+                "amount": 123,
+                "size": "small"
+            },
+                {
+                "name": "green",
+                "amount": 5643,
+                "size": "medium"
+            }
+        ];
+        const response = await axios.post(
+            domain + resource, 
+            body,
+            {
+                headers: {
+                  'Content-Type': 'application/json',
+                  Accept: 'application/json',
+                },
+            },
+        );
+        
+        expect(response.status).toEqual(200);
+        expect(response.data["failedCount"]).toEqual(0);
+    });
+
+    it("GET /events with success", async () => {
+        const response = await axios.get( domain + resource);
+
+        expect(response.status).toEqual(200);
+        expect(response.data["data"]).toEqual(          
+            expect.arrayContaining([
+                expect.objectContaining({"name": "blue", "amount": 123, "size": "small"}),
+                expect.objectContaining({"name": "green", "amount": 5643, "size": "medium"}),
+            ])
+        )
+    });
+});
